262dadbc1818c4714b4f464789440dd182703c14
Leonardo Maragna <86269559+LeonardoMaragna11@users.noreply.github.com>
2021-11-28 19:52:56 -0300

    Update db_proj.sql

--- 2/db_proj.sql
+++ 2/db_proj.sql
@@ -1,13 +1,16 @@
+-- CREATE DATABASE `db_proj`;
+-- USE `db_proj`;
+-- CREATE TABLE IF NOT EXISTS `db_proj`.`tb_usuario` ( 
+--     `cod_us` INT NOT NULL AUTO_INCREMENT, 
+--     `nome_us` VARCHAR(45) NULL, 
+--     `login_us` VARCHAR(45) NULL, 
+--     `senha_us` VARCHAR(45) NULL unique, 
+--     `nivel_us` CHAR NOT NULL,
+--     PRIMARY KEY (`cod_us`)) ENGINE = InnoDB;
 
--- -- CREATE TABLE IF NOT EXISTS `db_proj`.`tb_usuario` ( 
--- --     `cod_us` INT NOT NULL AUTO_INCREMENT, 
--- --     `nome_us` VARCHAR(45) NULL, 
--- --     `login_us` VARCHAR(45) NULL, 
--- --     `senha_us` VARCHAR(45) NULL unique, 
--- --     `nivel_us` CHAR NOT NULL,
--- --     PRIMARY KEY (`cod_us`)) ENGINE = InnoDB;
--- -- INSERT INTO tb_usuario(nome_us, login_us, senha_us,nivel_us) 
+-- USE `db_proj`;
+-- INSERT INTO `tb_usuario`(nome_us, login_us, senha_us,nivel_us) 
 --         VALUES ("Renato","renato@gmail.com" ,md5("1234"),'1');
--- -- INSERT INTO tb_usuario(nome_us, login_us, senha_us, nivel_us) 
+-- INSERT INTO `tb_usuario`(nome_us, login_us, senha_us, nivel_us) 
 --         VALUES ("Leonardo","leo@gmail.com" ,md5("11"),'2');
--- -- SELECT * FROM `tb_usuario`
+-- SELECT * FROM `tb_usuario`
