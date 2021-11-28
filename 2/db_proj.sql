5004069e4af1b41f722cb5b78546338933b5f7c0
Leonardo Maragna <86269559+LeonardoMaragna11@users.noreply.github.com>
2021-11-28 19:45:07 -0300

    Criando o BD

--- 2/db_proj.sql
+++ 2/db_proj.sql
@@ -1,10 +1,11 @@
 
-CREATE TABLE IF NOT EXISTS `miniPhp`.`tb_usuario` ( 
-    `cod_us` INT NOT NULL AUTO_INCREMENT, 
-    `nome_us` VARCHAR(45) NULL, 
-    `login_us` VARCHAR(45) NULL, 
-    `senha_us` VARCHAR(45) NULL unique, 
-    PRIMARY KEY (`cod_us`)) ENGINE = InnoDB;
-INSERT INTO TB_USUARIO(NOME_US, LOGIN_US, SENHA_US) VALUES ("Renato","renato@gmail.com" ,md5("1234"));
-INSERT INTO TB_USUARIO(NOME_US, LOGIN_US, SENHA_US) VALUES ("Leonardo","leo@gmail.com" ,md5("11"));
-SELECT * FROM `tb_usuario`
+-- CREATE TABLE IF NOT EXISTS `db_proj`.`tb_usuario` ( 
+--     `cod_us` INT NOT NULL AUTO_INCREMENT, 
+--     `nome_us` VARCHAR(45) NULL, 
+--     `login_us` VARCHAR(45) NULL, 
+--     `senha_us` VARCHAR(45) NULL unique, 
+--     `nivel` CHAR NOT NULL,
+--     PRIMARY KEY (`cod_us`)) ENGINE = InnoDB;
+-- INSERT INTO tb_usuario(nome_us, login_us, senha_us) VALUES ("Renato","renato@gmail.com" ,md5("1234"));
+-- INSERT INTO tb_usuario(nome_us, login_us, senha_us) VALUES ("Leonardo","leo@gmail.com" ,md5("11"));
+-- SELECT * FROM `tb_usuario`
